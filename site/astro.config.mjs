767210142236d825458fed6169dfb43a98f9f413
cdxker <fdenzell@gmail.com>
2025-06-21 11:31:24 -0700

    feat: allowedHosts in config

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -4,4 +4,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     "site": "https://fucking-music.com",
+    server: {
+        host: true,
+        allowedHosts: ["fuckingmusic.com", "www.fuckingmusic.com"]
+    }
 });
