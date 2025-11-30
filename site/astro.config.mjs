517f56567908f79ef493f2b6223a06892309999e
cdxker <32852291+cdxker@users.noreply.github.com>
2025-11-30 14:01:00 -0800

    Revert "feature: added PlayerView rough draft"

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,22 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
-
-import tailwindcss from "@tailwindcss/vite";
-
 // https://astro.build/config
 export default defineConfig({
-  "site": "https://fucking-music.com",
-
-  server: {
-      host: true,
-      allowedHosts: ["fuckingmusic.com", "www.fuckingmusic.com"]
-  },
-
-  integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    "site": "https://fucking-music.com",
+    server: {
+        host: true,
+        allowedHosts: ["fuckingmusic.com", "www.fuckingmusic.com"]
+    }
 });
