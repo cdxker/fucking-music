b60acab1521a0fbec2ed47b151b5e0617b58567d
cdxker <fdenzell@gmail.com>
2025-11-30 13:25:12 -0800

    feature: added PlayerView rough draft

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,11 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-    "site": "https://fucking-music.com",
-    server: {
-        host: true,
-        allowedHosts: ["fuckingmusic.com", "www.fuckingmusic.com"]
-    }
+  "site": "https://fucking-music.com",
+
+  server: {
+      host: true,
+      allowedHosts: ["fuckingmusic.com", "www.fuckingmusic.com"]
+  },
+
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
---
+++ site/src/pages/PlayerView.tsx
@@ -0,0 +1,54 @@
+type PlaylistId = `play-${string}`;
+
+interface Playlist {
+  id: PlaylistId;
+  track_cover_uri: string;
+  first_track: TrackId;
+  artists: string[];
+}
+
+type TrackId = `track-${string}`;
+
+interface Track {
+  id: TrackId;
+  time_ms: number;
+  name: string;
+  artists: string[];
+  tags?: string[];
+  next_tracks?: Record<PlaylistId, TrackId>;
+}
+
+function PlaylistView({ playlist }: { playlist: Playlist }) {
+  return (
+    <div>
+      <div>
+        <img src={playlist.track_cover_uri} />
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+function PlayerView() {
+  const Fetish: Playlist = {
+    id: "play-f",
+    track_cover_uri: "../public/covers__fetish.png",
+    artists: ["Oblique Occasions"],
+    first_track: "track-i-dream",
+  };
+
+  return (
+    <div>
+      <div className="flex justify-between">
+        <div>
+          <div className="text-5xl opacity-50 text-white">less</div>
+        </div>
+        <div className="text-5xl opacity-50 text-white">more?</div>
+      </div>
+      <div className="italic text-5xl opacity-50 text-white">fucking music</div>
+      <PlaylistView playlist={Fetish} />
+    </div>
+  );
+}
+
+export default PlayerView;
